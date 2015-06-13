15df085a8544bde0a46ca71b14ebee0c7eeacdc2
Chuck Clift <chkengr@gmail.com>
2015-06-13 16:56:10 -0400

    added get_urls function
    This function takes the html of a web page and extracts
    all of the urls inside of the anchor tags

--- htmlParser.py
+++ htmlParser.py
@@ -1,5 +1,44 @@
+#!/usr/bin/python3
+import re
 
-def getParagraphs(html):    
+def get_urls(html):
+    regex = "<a.*?>"
+    anchor_list = re.findall(regex, html)
+    html_urls = []
+    
+    # getting the urls from the html document    
+    for a in anchor_list:
+        url_beginning = a.find("href=") + 6
+ 
+        if url_beginning is not -1:
+            _ = a[url_beginning:-1]
+            url_end = _.find("\"")
+            url = _[0:url_end]
+            html_urls.append(url)
+
+    # filtering out the "#" values
+    html_urls = [x for x in html_urls if x.strip() is not "#"]
+
+
+    # filtering out all values that don't start with "http" or
+    # "/" so that only values like these remain
+    #        "https://www.whatever.com
+    #        "/relative/link/
+
+    for i in html_urls:
+        # filtering out all values that don't start with 
+        # "http" or "/"
+        http_at_start = i.startswith("http")
+        slash_at_start = i.startswith("/")
+
+       
+        if not http_at_start and not slash_at_start:
+            html_urls.remove(i) 
+
+    return html_urls 
+        
+    
+def get_paragraphs(html):    
     pContent = ""
 
     ##getting everything that is inside of the p tags
@@ -35,6 +74,9 @@ def getParagraphs(html):
 
     
 if __name__ == "__main__":
-    with open("html.txt", encoding="utf-8") as f:
-        html = f.read()
-    print(getParagraphs(html))
+    with open("example.html", encoding="utf-8") as f:
+        sampleHtml = f.read()
+    url_list = get_urls(sampleHtml) 
+    for a in url_list:
+        print(a)
+    
